7af6e7107edac4cb035ff46ec30d10a8ab514fa8
IMAndzia <62576098+IMAndzia@users.noreply.github.com>
2025-03-25 21:46:32 -0400

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package Beginners;
+
+import java.util.Scanner;
+
+
+public class Helloworld {
+public static void main (String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Hello World!");
+
+}
+}
